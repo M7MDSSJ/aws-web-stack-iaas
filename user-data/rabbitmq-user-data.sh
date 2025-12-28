a1d6e38ef73344695ee8cbae9e698c4b3f5b0e56
Mohamed Ismail <118367271+M7MDSSJ@users.noreply.github.com>
2025-12-29 00:11:25 +0200

    Add RabbitMQ setup script
    This script sets up RabbitMQ by importing signing keys, adding the repository, installing dependencies, enabling the service, and configuring a user.

---
+++ user-data/rabbitmq-user-data.sh
@@ -0,0 +1,27 @@
+```bash
+#!/bin/bash
+## Import RabbitMQ signing keys
+rpm --import 'https://github.com/rabbitmq/signing-keys/releases/download/3.0/rabbitmq-release-signing-key.asc'
+rpm --import 'https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-erlang.E495BB49CC4BBE5B.key'
+rpm --import 'https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-server.9F4587F226208342.key'
+
+## Add RabbitMQ repository
+curl -o /etc/yum.repos.d/rabbitmq.repo https://raw.githubusercontent.com/hkhcoder/vprofile-project/refs/heads/awsliftandshift/al2023rmq.repo
+
+dnf update -y
+
+## Install dependencies and RabbitMQ server
+dnf install socat logrotate -y
+dnf install -y erlang rabbitmq-server
+
+## Enable and start RabbitMQ
+systemctl enable rabbitmq-server
+systemctl start rabbitmq-server
+
+## Configure RabbitMQ
+sudo sh -c 'echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config'
+sudo rabbitmqctl add_user test test
+sudo rabbitmqctl set_user_tags test administrator
+rabbitmqctl set_permissions -p / test ".*" ".*" ".*"
+
+sudo systemctl restart rabbitmq-server
