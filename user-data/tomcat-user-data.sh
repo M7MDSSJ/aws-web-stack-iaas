113597d419b5650e67809a6a973be8cc8389fd44
Mohamed Ismail <118367271+M7MDSSJ@users.noreply.github.com>
2025-12-29 00:09:57 +0200

    Add user-data script for Tomcat setup
    Initial setup script for Tomcat and Java installation.

---
+++ user-data/tomcat-user-data.sh
@@ -0,0 +1,8 @@
+```bash
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt install openjdk-17-jdk -y
+sudo apt install tomcat10 tomcat10-admin tomcat10-docs tomcat10-common git -y
+
+# Additional commands to pull and deploy artifact from S3 can be added here
